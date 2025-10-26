cb5e3fa62e919d01f9ea325ce1439f589f2d2855
pdefusco <pauldefusco@gmail.com>
2025-10-25 19:55:01 -0700

    completed spatial joins

--- code/spatial_joins.py
+++ code/spatial_joins.py
@@ -72,20 +72,73 @@ neighborhoods_path = "/app/mount/las_vegas_neighborhoods.csv"
 
 # Read and show accidents
 df_accidents = read_sedona_csv(accidents_path, None)
+df.createOrReplaceTempView("accidents")
 df_accidents.show()
 
 # Read and show street network
 df_streets = read_sedona_csv(streets_path, None)
+df.createOrReplaceTempView("streets")
 df_streets.show()
 
 # Read and show points of interest
 df_pois = read_sedona_csv(pois_path, None)
+df.createOrReplaceTempView("pois")
 df_pois.show()
 
 # Read and show accidents
 df_neighborhoods = read_sedona_csv(neighborhoods_path, None)
+df.createOrReplaceTempView("neighborhoods")
 df_neighborhoods.show()
 
 # Read and show accidents
 df_pii = read_sedona_csv(accidents_path, None)
+df.createOrReplaceTempView("pii")
 df_pii.show()
+
+# SPATIAL JOINS
+
+# Find all points within the provided polygons
+# Find all accidents taking place in each neighborhood
+#
+sedona.sql(
+    """SELECT
+        accidents.accident_id as accident_id,
+        neighborhoods.name as neighborhood
+    FROM accidents
+    JOIN neighborhoods ON ST_Within(accidents.wkt, neighborhoods.geometry);"""
+).show()
+
+# Find all lines within the provided polygons
+# Find all roads travelling through each neighborhood
+#
+sedona.sql(
+    """SELECT
+        streets.name as street_name,
+        neighborhoods.name as neighborhood
+    FROM streets
+    LEFT JOIN neighborhoods ON ST_Crosses(streets.wkt, neighborhoods.geometry);"""
+).show()
+
+# Spatial join K-nearest neighbors
+# For each accident location find the two nearest points of interest
+#
+sedona.sql(
+    """SELECT
+        accidents.accident_id as accident_id,
+        pois.name AS poi_name
+    FROM accidents
+    JOIN pois
+    ON ST_KNN(accidents.wkt, pois.geometry, 2)"""
+).show()
+
+# Spatial distance join
+# For each accident find all the points of interest that are within 2.5 kilometers of the point
+#
+sedona.sql(
+    """SELECT
+        accidents.accident_id as accident_id,
+        pois.name AS poi_name
+    FROM accidents
+    JOIN pois
+    ON ST_DWithin(accidents.geometry, pois.geometry, 2500, useSpheroid = true)"""
+).show()
