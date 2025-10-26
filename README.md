0c476a88b539018ac5f3b912e2dfa7e15f1ab3d2
pdefusco <pauldefusco@gmail.com>
2025-10-25 18:40:08 -0700

    project skeleton

--- README.md
+++ README.md
@@ -1,1 +1,132 @@
-# CDE_Spark_Sedona_Spatial_Joins
+# Spark Sedona Spatial Joins in Cloudera Data Engineering
+
+
+
+
+## CDE Setups
+
+Create CDE Files Resource and upload scripts and data
+
+```
+cde resource create --type file \
+  --name data
+
+cde resource upload --name data \
+  --local-path code/spatial_joins.py \
+  --local-path data/las_vegas_accidents.csv \
+  --local-path data/las_vegas_customer_pii.csv \
+  --local-path data/las_vegas_neighborhoods.csv \
+  --local-path data/las_vegas_pois.csv \
+  --local-path data/las_vegas_streets.csv
+```
+
+Create CDE Python Resource
+
+```
+cde resource create --type python \
+  --name sedona
+
+cde resource upload --name sedona \
+  --local-path sedona
+```
+
+## CDE Spark Connect Docker Container Setups
+
+The demo is containerized.
+
+#### Pull the Docker Container and Launch the IDE
+
+Clone the GitHub repository in your local machine.
+
+```
+git clone https://github.com/pdefusco/CDE_Spark_Sedona_Spatial_Joins.git
+cd CDE_Spark_Sedona_Spatial_Joins
+```
+
+Launch the Docker container.
+
+```
+docker run -p 8888:8888 pauldefusco/cde_spark_sedona_spatial_joins
+```
+
+Launch the JupyterLab IDE in your browser by copy and pasting the provided url as shown below.
+
+![alt text](../../img/docker-container-launch.png)
+
+You now have access to all lab materials from the JupyterLab IDE in the left pane. From here, you can launch notebooks and run the terminal.
+
+![alt text](../../img/jl-home.png)
+
+You will use the terminal in the IDE to run the CDE CLI commands for the labs. First you need to configure the CLI and install Spark Connect though.
+
+#### Configure the CDE CLI and Install Spark Connect for CDE.
+
+Open CDE's configurations and apply your Workload Username and Jobs API URL. You can find your Jobs API URL in your Virtual Cluster's Details Page.
+
+![alt text](../../img/jobs-api-url-1.png)
+
+![alt text](../../img/jobs-api-url-2.png)
+
+![alt text](../../img/cli-configs-1.png)
+
+![alt text](../../img/cli-configs-2.png)
+
+Next, generate a CDP access token and edit your CDP credentials.
+
+![alt text](../../img/usr-mgt-1.png)
+
+![alt text](../../img/usr-mgt-2.png)
+
+![alt text](../../img/usr-mgt-3.png)
+
+![alt text](../../img/cdp-credentials.png)
+
+Finally, run the following commands to install the CDE Spark Connect tarballs.
+
+```
+pip3 install cdeconnect.tar.gz  
+pip3 install pyspark-3.5.1.tar.gz
+```
+
+![alt text](../../img/install-deps.png)
+
+#### Launch a CDE Spark Connect Session
+
+Start a CDE Session of type Spark Connect. Edit the Session Name parameter so it doesn't collide with other users' sessions. You will be prompted for your Workload Password. This is the same password you used to log into CDP.
+
+```
+cde session create \
+  --name geospatial-session \
+  --type spark-connect \
+  --num-executors 2 \
+  --driver-cores 2 \
+  --driver-memory "4g" \
+  --executor-cores 4 \
+  --executor-memory "8g"
+```
+
+![alt text](../../img/launchsess.png)
+
+In the Sessions UI, validate the Session is Running.
+
+![alt text](../../img/cde_session_validate_1.png)
+
+![alt text](../../img/cde_session_validate_2.png)
+
+#### Run Your First PySpark & Iceberg Application via Spark Connect
+
+You are now ready to connect to the CDE Session from your local JupyterLab IDE using Spark Connect.
+
+Open Iceberg_TimeTravel_PySpark.ipynb. Update the Spark Connect session name, the username and the Storage Location variables in the first two cells. Then run each cell in the notebook.
+
+```
+from cde import CDESparkConnectSession
+spark = CDESparkConnectSession.builder.sessionName('<your-spark-connect-session-name-here>').get()
+```
+
+```
+storageLocation = <your-storage-location-here>
+username = <your-cdp-workload-username-here>
+```
+
+![alt text](../../img/runnotebook-1.png)
---
+++ code/spatial_joins.py
@@ -0,0 +1,91 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2025
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+from sedona.register import SedonaRegistrator
+from sedona.utils import SedonaKryoRegistrator, KryoSerializer
+from sedona.sql.types import GeometryType
+import pyspark.sql.functions as F
+from sedona.spark import *
+from pyspark.sql import SparkSession
+
+# Create a Spark session with Sedona configuration
+config = SedonaContext.builder().\
+    config('spark.jars.packages',
+           'org.apache.sedona:sedona-spark-shaded-3.5_2.12:1.7.0,'
+           'org.datasyslab:geotools-wrapper:1.7.0-28.5').\
+    getOrCreate()
+sedona = SedonaContext.create(config)
+
+# Function to read CSV and convert WKT to geometry column
+def read_sedona_csv(filepath, schema_cols):
+    df = sedona.read.csv(filepath, header=True, inferSchema=True)
+    # Convert WKT column to geometry
+    df = df.withColumn("geometry", F.expr("ST_GeomFromWKT(wkt)"))
+    # Show schema and first rows
+    print(f"Data from {filepath}:")
+    df.printSchema()
+    df.show(5, truncate=False)
+    return df
+
+# Paths to your files
+pii_path = "/app/mount/las_vegas_customer_pii.csv"
+accidents_path = "/app/mount/las_vegas_accidents.csv"
+streets_path = "/app/mount/las_vegas_streets.csv"
+pois_path = "/app/mount/las_vegas_casino_pois.csv"
+neighborhoods_path = "/app/mount/las_vegas_neighborhoods.csv"
+
+# Read and show accidents
+df_accidents = read_sedona_csv(accidents_path, None)
+df_accidents.show()
+
+# Read and show street network
+df_streets = read_sedona_csv(streets_path, None)
+df_streets.show()
+
+# Read and show points of interest
+df_pois = read_sedona_csv(pois_path, None)
+df_pois.show()
+
+# Read and show accidents
+df_neighborhoods = read_sedona_csv(neighborhoods_path, None)
+df_neighborhoods.show()
+
+# Read and show accidents
+df_pii = read_sedona_csv(accidents_path, None)
+df_pii.show()
